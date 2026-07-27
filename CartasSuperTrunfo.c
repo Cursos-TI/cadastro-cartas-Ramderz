d9ace0c33c81977baceb7df77c5e585a62a1fa6c
Ramderz <85895601+Ramderz@users.noreply.github.com>
2026-07-27 21:04:45 +0000

    finalizado desafio mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,12 +9,20 @@ int main() {
      char estadoA[50], estadoB[50];
      char codigoA[50], codigoB[50];
      char nomecidadeA[50], nomecidadeB[50];
-     int populacaoA, populacaoB;
+     unsigned long int populacaoA, populacaoB;
      float areaA, areaB;
      float pibA, pibB;
      int pontoturisticoA, pontoturisticoB;
      float densidadeA, densidadeB;
      float pibpercapitaA, pibpercapitaB;
+     float SuperTrunfoA, SuperTrunfoB;
+     int Rpopulacao;
+     int Rarea;
+     int Rpib;
+     int Rdensidade;
+     int Rpibpercapita;
+     int Rsupertrunfo;
+     int Rpontoturistico;
       
         // Entrada de dados para a cidade A
         printf("Digite o nome do estado A: \n");
@@ -24,7 +32,7 @@ int main() {
         printf("Digite o nome da cidade A: \n");
         scanf("%s", nomecidadeA);
         printf("Digite a populacao da cidade A: \n");
-        scanf("%d", &populacaoA);
+        scanf("%lu", &populacaoA);
         printf("Digite a area da cidade A: \n");
         scanf("%f", &areaA);
         printf("Digite o PIB da cidade A: \n");
@@ -43,7 +51,7 @@ int main() {
         printf("Digite o nome da cidade B: \n");
         scanf("%s", nomecidadeB);
         printf("Digite a populacao da cidade B: \n");
-        scanf("%d", &populacaoB);
+        scanf("%lu", &populacaoB);
         printf("Digite a area da cidade B: \n");
         scanf("%f", &areaB);
         printf("Digite o PIB da cidade B: \n");
@@ -60,7 +68,7 @@ int main() {
         printf("Estado: %s \n", estadoA);
         printf("Codigo: %s \n", codigoA);
         printf("Nome da Cidade: %s \n", nomecidadeA);
-        printf("Populacao: %d \n", populacaoA);
+        printf("Populacao: %lu \n", populacaoA);
         printf("area: %.2f \n", areaA);
         printf("PIB: %.2f \n", pibA);
         printf("Numero de Pontos Turisticos: %d \n", pontoturisticoA);
@@ -71,12 +79,35 @@ int main() {
         printf("Estado: %s \n", estadoB);
         printf("Codigo: %s \n", codigoB);
         printf("Nome da Cidade: %s \n", nomecidadeB);
-        printf("Populacao: %d \n", populacaoB);
+        printf("Populacao: %lu \n", populacaoB);
         printf("area: %.2f \n", areaB);
         printf("PIB: %.2f \n", pibB);
         printf("Numero de Pontos Turisticos: %d \n", pontoturisticoB);
         printf("Densidade: %.2f \n", densidadeB);
         printf("PIB per capita: %.2f \n", pibpercapitaB);
+
+      // Comparação das cartas
+        Rpopulacao = populacaoA > populacaoB;
+        Rarea = areaA > areaB;
+        Rpib = pibA > pibB;
+        Rpontoturistico = pontoturisticoA > pontoturisticoB;
+        Rdensidade = densidadeA > densidadeB;
+        Rpibpercapita = pibpercapitaA > pibpercapitaB;
+        SuperTrunfoA = populacaoA + areaA + pibA + pontoturisticoA + (densidadeA / 1) + pibpercapitaA;
+        SuperTrunfoB = populacaoB + areaB + pibB + pontoturisticoB + (densidadeB / 1) + pibpercapitaB;
+        Rsupertrunfo = SuperTrunfoA > SuperTrunfoB;
+
+        // Saida de dados de compração
+        printf("Comparacao de cartas: \n");
+        printf("Populacao: %d \n", Rpopulacao);
+        printf("Area: %d \n", Rarea);
+        printf("PIB: %d \n", Rpib);
+        printf("Numero de Pontos Turisticos: %d \n", Rpontoturistico);
+        printf("Densidade: %d \n", Rdensidade);
+        printf("PIB per capita: %d \n", Rpibpercapita);
+        printf("Super trunfo : %d \n", Rsupertrunfo);
+
+
 
         return 0;
 
